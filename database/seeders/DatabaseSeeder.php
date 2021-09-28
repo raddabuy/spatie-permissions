560d73e6a16c7bc44dc7827f15092f45b8a4f606
raddabuy <raddabuy@yandex.ru>
2021-09-28 11:59:32 +0500

    uncomment seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,11 +15,11 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
         $this->call([
-//            UserSeeder::class,
-//            RoleSeeder::class,
-//            PermissionSeeder::class
-//            PostsSeeder::class
-//            RoleHasPermissionsSeeder::class
+            UserSeeder::class,
+            RoleSeeder::class,
+            PermissionSeeder::class,
+            PostsSeeder::class,
+            RoleHasPermissionsSeeder::class,
             UserRoleSeeder::class
         ]);
     }
